d14303953467a20d6d4557b2dbeaf60a40a8ca02
BEZMOZGLIY <igordears@gmail.com>
2023-06-10 13:39:48 +0300

    First commit

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+(function(){
+    "use strict";
+    class Games {
+        constructor(name, hours) {
+            this.name = name;
+            this.hours = hours;
+        }    
+    }
+let game = new Games('MHw',7);
+console.log(game);
+
+
+})(); 
