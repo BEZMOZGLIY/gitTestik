93a841365ec939de70fd5dde861bc8eef9d15717
BEZMOZGLIY <igordears@gmail.com>
2023-06-14 13:04:17 +0300

    F2ck

--- JavaScript.js
+++ JavaScript.js
@@ -12,4 +12,6 @@ let DarkSouls2 = new Games("Dark Souls II",891)
 console.log(DarkSouls2);
 let DarkSouls3 = new Games("Dark Souls III",1000);
 console.log(DarkSouls3);
-})(); 
+})();
+let DarkSoulsRemastered = new Games("Dark Souls Remastered",1000);
+console.log(DarkSouls3);
